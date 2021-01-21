cdcbea71c4598362702d5ec525fe1d4b2bb90a58
Guizombas <guilherme.gabriel.532@gmail.com>
2021-01-21 20:59:54 -0300

    integrces front back em login e cadastro

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,18 +3,90 @@ import {Link} from 'react-router-dom'
 import {FaLongArrowAltLeft} from 'react-icons/fa'
 
 import "../styles/pages/loginCadastro.css"
+import { FormEvent } from 'react'
 
 const Login = () => {
+
+    const handleLoginSubmit = (event: FormEvent) =>{
+
+        event.preventDefault()
+
+        let div:any;
+
+        //reseta estilização
+        div = document.querySelector(' input[name="password"] + .errWarning ')
+        div.style.display = 'none'
+        div = document.querySelector(' input[name="email"] + .errWarning ')
+        div.style.display = 'none'
+
+        div = document.getElementsByName('email')[0]
+        const email = div.value
+        div = document.getElementsByName('password')[0]
+        const password = div.value
+
+        const data = {
+            'email':email,
+            'password':password
+        }
+
+            
+        const reqConfig:RequestInit = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+
+        }
+
+        fetch( 'http://localhost:3300/login', reqConfig )
+        .then( res =>{
+
+            if (res.status === 200)
+                return res.json()
+
+            if (res.status === 404)
+                div = document.querySelector(' input[name="email"] + .errWarning ')
+            else
+                div = document.querySelector(' input[name="password"] + .errWarning ')
+            
+            div.style.display = 'block'
+
+        } )
+        .then( res =>{
+
+            if (res){
+                //controle de sessão
+                window.location.href = '/menu'
+            }
+
+        } )
+        .catch( err =>{
+            console.log(err);
+            alert('Erro no servidor')
+            
+        })
+    }
+    
+
     return(
         <div id="loginPage">
-            <form action="/menu">
+
+            <form onSubmit={handleLoginSubmit}>
+
                 <Link to="/"><FaLongArrowAltLeft className="backArrow"></FaLongArrowAltLeft></Link>
                 <h1>HowRU</h1>
                 <div className="inputs">
+
                     <div className="fieldName">e-mail:</div>
-                    <input type="email" placeholder="e-mail"/>
+                    <input name="email" type="email" placeholder="e-mail" required/>
+                    <small className="errWarning">E-mail não registrado</small>
+
                     <div className="fieldName">senha:</div>
-                    <input type="password" placeholder="senha" autoComplete="off"/>
+                    <input name="password" type="password" placeholder="senha" autoComplete="off" required/>
+                    <small className="errWarning">Senha incorreta</small>
+
+
                 </div>    
                 <button type="submit">Logar</button>           
                 
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -9,30 +9,74 @@ import "../styles/pages/loginCadastro.css"
 const Register = () => {
 
     const handleRegisterSubmit = (event: FormEvent) =>{
+        event.preventDefault()
 
         let div:any
 
-        div = document.getElementsByName('email')[0]
-        const email = div.value
+        //garante formatação limpa
+        div = document.querySelector(' input[name="rpassword"] + .errWarning ')
+        div.style.display = 'none'
+        div = document.querySelector(' input[name="email"] + .errWarning ')
+        div.style.display = 'none'
+
         div = document.getElementsByName('name')[0]
         const name = div.value
+        div = document.getElementsByName('email')[0]
+        const email = div.value
         div = document.getElementsByName('password')[0]
         const password = div.value
         div = document.getElementsByName('rpassword')[0]
         const rpassword = div.value
 
+        if (password!==rpassword){
+            div = document.querySelector(' input[name="rpassword"] + .errWarning ')
+            div.style.display = 'block'
+            return
+        }
+
+        const data = {
+            'name':name,
+            'email':email,
+            'password':password
+        }
         
+        
+        const reqConfig:RequestInit = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
 
-
+        }
             
+        fetch( 'http://localhost:3300/register', reqConfig )
+        .then( res =>{
+            
+            if (res.status === 201)
+                return res.json()
+
+            div = document.querySelector(' input[name="email"] + .errWarning ')
+            div.style.display = 'block'
+            
+        })
+        .then( jsonRes=>{
+            if (jsonRes){
+                //fazer controle de sessão
+                window.location.href = "/menu"
+            }
+        })
+        .catch( err =>{
+            console.log(err);
+            alert("Erro no servidor")
+        })
         
 
-        event.preventDefault()
     }
 
     return(
         <div id="registerPage">
-            <form onSubmit={handleRegisterSubmit} action="/menu">
+            <form id="registerForm" onSubmit={handleRegisterSubmit} action="/menu">
 
                 <Link to="/"><FaLongArrowAltLeft className="backArrow"></FaLongArrowAltLeft></Link>
                 <h1>HowRU</h1>
@@ -48,6 +92,7 @@ const Register = () => {
                         required
                         maxLength={50}
                     />
+                    <small className="errWarning">E-mail já registrado</small>
 
                     <div className="fieldName">nome</div>
                     <input 
@@ -58,7 +103,6 @@ const Register = () => {
                         required
                         maxLength={50}
                     />
-                    <small>* Pode ser alterado posteriormente</small>
 
                     <div className="fieldName">senha:</div>
                     <input 
@@ -67,7 +111,10 @@ const Register = () => {
                         placeholder="senha" 
                         autoComplete="off"
                         required
+                        minLength={6}
                         maxLength={30}
+                        pattern="[A-Za-z0-9]{6,}"
+                        title="Pelo menos 6 caracteres alfanuméricos"
                     />
 
                     <div className="fieldName">confirmação da senha:</div>
@@ -79,6 +126,8 @@ const Register = () => {
                         required
                         maxLength={30}
                     />
+                    <small className="errWarning">Senhas não correspondem</small>
+
                 </div>    
                 <button type="submit">Cadastrar</button>           
                 
