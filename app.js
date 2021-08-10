b3a668d78228db78cc719d0c7b601af69bfda4e8
guizombas <63319368+guizombas@users.noreply.github.com>
2021-08-10 09:12:05 -0300

    Publish

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const root = path.join(__dirname, 'build')
 
 // Constants
 const PORT = process.env.PORT || 80;
@@ -14,10 +15,9 @@ const PORT = process.env.PORT || 80;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(root));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile('index.html', {root, path:''});
 });
 
 app.listen(PORT, () => {
